855a028e1b219f4bdccbcc1df3d29652a7a93014
Richard-de-Souza <rickpili22@gmail.com>
2023-07-26 14:29:23 -0300

    commit aula 456

--- contact/admin.py
+++ contact/admin.py
@@ -4,4 +4,9 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    ...
+    list_display = 'id', 'first_name', 'last_name', 'phone',
+    ordering = 'id',
+    search_fields = 'id', 'first_name', 'last_name',
+    list_per_page = 1
+    list_max_show_all = 200
+    list_display_links = 'id',
