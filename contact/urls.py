e1cbfd418bc715eeb26661b95b19180424d34fc2
Richard-de-Souza <rickpili22@gmail.com>
2023-08-01 15:42:47 -0300

    criação do foms

--- contact/urls.py
+++ contact/urls.py
@@ -5,7 +5,13 @@ from contact import views
 app_name = 'contact'
 
 urlpatterns = [
-    path('<int:contact_id>/', views.contact, name='contact'),
     path('search/', views.search, name='search'),
     path('', views.index, name='index'),
+
+
+    path('<int:contact_id>/detail/', views.contact, name='contact'),
+    path('contact/create/', views.create, name='create'),
+    # path('<int:contact_id>/update/', views.contact, name='contact'),
+    # path('<int:contact_id>/delete/', views.contact, name='contact'),
+
 ]
---
+++ contact/views/contact_forms.py
@@ -0,0 +1,50 @@
+from typing import Any, Dict
+from django.core.paginator import Paginator
+from django.db.models import Q
+from django.shortcuts import get_object_or_404, redirect, render
+from django import forms
+from django.core.exceptions import ValidationError
+
+from contact.models import Contact
+
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = (
+            'first_name', 'last_name', 'phone',)
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+
+        self.add_error(
+            'first_name',
+            ValidationError(
+                'Mensagem de erro',
+                code='invalid'
+            )
+        )
+        return super().clean()
+
+
+def create(request):
+    if request.method == 'POST':
+        context = {
+            'form': ContactForm(request.POST)
+        }
+
+        return render(
+            request,
+            'contact/create.html',
+            context
+        )
+
+    context = {
+        'form': ContactForm()
+    }
+
+    return render(
+        request,
+        'contact/create.html',
+        context
+    )
